6e39305f8e3c40a1ac53fe42daa69ac9c08ac33d
Shaun Luttin <shaun.luttin@mediavalet.com>
2024-02-08 15:55:18 -0800

    Introduce Start-Project.ps1

---
+++ my-hugo-blog/_apps/wasm-game-of-life/Start-Project.ps1
@@ -0,0 +1,8 @@
+# This prevents node from complaining about SSL crypto stuff.
+$env:NODE_OPTIONS = "--openssl-legacy-provider"
+
+# Open the browser automatically.
+Start-Process "http://localhost:8080/";
+
+# This starts the project in the www/ directory.
+npm run start --prefix .\www\
